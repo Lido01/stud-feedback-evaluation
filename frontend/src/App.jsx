87813789c77193c6527440429a0701fd45d36d6d
Lidetu <lidosinafanis@gmail.com>
2026-04-23 20:45:53 +0300

    setting up frontend with folder structure defined

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1>Feedback Evaluation System</h1>
+      <p>Welcome to the Feedback Evaluation System. Please log in to continue.</p>
+    </>
+  )
+}
+
+export default App
